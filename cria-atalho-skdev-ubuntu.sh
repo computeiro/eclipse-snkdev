eb2df03940ebb71c1940b517b639702753e67ed8
wender <wender4j@gmail.com>
2021-07-16 20:21:07 -0300

    Documentando e padronizando projeto

--- cria-atalho-skdev-ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ ! -d /opt/eclipse-snk ]];
-then
-    sudo mkdir /opt/eclipse-snk
-fi
-
-sudo wget -P /opt/eclipse-snk/  https://raw.githubusercontent.com/computeiro/snk-docker/main/docs/eclipse-icon.xpm
-sudo wget -P /usr/share/applications https://raw.githubusercontent.com/computeiro/snk-docker/main/docs/eclipse-snk.desktop
