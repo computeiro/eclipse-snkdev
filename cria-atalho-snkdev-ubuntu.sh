273fd80fe205a2de0f9efe68395294fe35c87e72
Wender de Oliveira <wender4j@gmail.com>
2021-08-13 15:22:20 -0300

    Update cria-atalho-snkdev-ubuntu.sh

--- cria-atalho-snkdev-ubuntu.sh
+++ cria-atalho-snkdev-ubuntu.sh
@@ -7,5 +7,5 @@ then
     sudo mkdir $optDir
 fi
 
-sudo wget -P $optdir  https://raw.githubusercontent.com/computeiro/eclipse-snkdev/main//docs/eclipse-icon.xpm
+sudo wget -P $optdir  https://raw.githubusercontent.com/computeiro/eclipse-snkdev/main/docs/eclipse-icon.xpm
 sudo wget -P /usr/share/applications https://raw.githubusercontent.com/computeiro/eclipse-snkdev/main/eclipse-snkdev.desktop
